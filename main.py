c6554164746c91ff1817ce62f48b6b696948172b
Lucas Mendes <111598752+lucasmdcv@users.noreply.github.com>
2025-03-09 15:33:48 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
-O NumPy é uma biblioteca fundamental para a computação científica com Python. Ela permite a realização de operações matemáticas e lógicas em grandes arrays e matrizes multidimensionais. Aqui estão algumas razões pelas quais o NumPy é amplamente utilizado:
+#O NumPy é uma biblioteca fundamental para a computação científica com Python. Ela permite a realização de operações matemáticas e lógicas em grandes arrays e matrizes multidimensionais. Aqui estão algumas razões pelas quais o NumPy é amplamente utilizado:
+#Eficiência: O NumPy é escrito em C, o que torna as operações com arrays muito mais rápidas do que as listas padrão do Python.
+#Funcionalidades: Oferece uma vasta gama de funções matemáticas e estatísticas, facilitando o trabalho com dados científicos.
+#Integração: Funciona bem com outras bibliotecas como SciPy, pandas, e Matplotlib, tornando-o ideal para projetos de ciência de dados e aprendizado de máquina.
+#Simplicidade: A sintaxe do NumPy é clara e concisa, permitindo a execução de operações complexas com poucas linhas de código.
 
-Eficiência: O NumPy é escrito em C, o que torna as operações com arrays muito mais rápidas do que as listas padrão do Python.
+import numpy as np
 
-Funcionalidades: Oferece uma vasta gama de funções matemáticas e estatísticas, facilitando o trabalho com dados científicos.
+# Criar um array de 1 dimensão
+array = np.array([1, 2, 3, 4, 5])
 
-Integração: Funciona bem com outras bibliotecas como SciPy, pandas, e Matplotlib, tornando-o ideal para projetos de ciência de dados e aprendizado de máquina.
+# Criar um array de 2 dimensões (matriz)
+matriz = np.array([[1, 2, 3], [4, 5, 6]])
 
-Simplicidade: A sintaxe do NumPy é clara e concisa, permitindo a execução de operações complexas com poucas linhas de código.
+print(array)
+print(matriz)
+
+
+

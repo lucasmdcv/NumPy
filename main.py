77a34055828c5fe0caf594bbe4b1dc0919296a82
Lucas Mendes <111598752+lucasmdcv@users.noreply.github.com>
2025-03-09 15:33:00 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+O NumPy é uma biblioteca fundamental para a computação científica com Python. Ela permite a realização de operações matemáticas e lógicas em grandes arrays e matrizes multidimensionais. Aqui estão algumas razões pelas quais o NumPy é amplamente utilizado:
+
+Eficiência: O NumPy é escrito em C, o que torna as operações com arrays muito mais rápidas do que as listas padrão do Python.
+
+Funcionalidades: Oferece uma vasta gama de funções matemáticas e estatísticas, facilitando o trabalho com dados científicos.
+
+Integração: Funciona bem com outras bibliotecas como SciPy, pandas, e Matplotlib, tornando-o ideal para projetos de ciência de dados e aprendizado de máquina.
+
+Simplicidade: A sintaxe do NumPy é clara e concisa, permitindo a execução de operações complexas com poucas linhas de código.
